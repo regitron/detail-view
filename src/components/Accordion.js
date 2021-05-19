f8331988c8a69f2fc4655d9c8dfb68bfb35cf24e
Reginald <reginald@dinamo.no>
2021-05-19 18:16:08 +0200

    added tabs

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -23,17 +23,57 @@ function Accordion(props) {
   );
 }
 
+// For each item in accordion
 function AccordionItem(props) {
   const movie = props.item;
   const [isHidden, setIsHidden] = useState(true);
   function toggleItem() {
     isHidden ? setIsHidden(false) : setIsHidden(true);
+    console.log(movie);
   }
   return (
-    <div key={movie.key} className={classes.item} onClick={toggleItem}>
-      <div className={classes.title}>#{parseInt(movie.key)+1} <span>{movie.display_title}</span></div>
-      <div className={isHidden ? classes.inner : classes.inner + ' ' + classes.show}>
-        <p>Tabs goes here</p>
+    <div key={movie.key} className={classes.item}>
+      <div className={classes.title} onClick={toggleItem}>
+        #{parseInt(movie.key) + 1} <span>{movie.display_title}</span>
+      </div>
+      <div
+        className={
+          isHidden ? classes.inner : classes.inner + " " + classes.show
+        }
+      >
+        <div className={classes.tab}>
+          <div className={classes.pills}>
+            <span className={classes.pill + " " + classes.active}>
+              Short summary
+            </span>
+            <span className={classes.pill}>Articles</span>
+            <span className={classes.pill}>Dates</span>
+          </div>
+          <div className={classes.contents}>
+            <div className={classes.content + " " + classes.active}>
+              <img alt={movie.headline} src={movie.multimedia.src} />
+              <div>
+                <h3>{movie.display_title}</h3>
+                <small>By: {movie.byline}</small>
+                <p>{movie.summary_short}</p>
+              </div>
+            </div>
+            <div className={classes.content}>
+              <p>
+                <a href={movie.link.url} target="_blank" rel="noreferrer">
+                  {movie.link.suggested_link_text}
+                </a>
+              </p>
+            </div>
+            <div className={classes.content + " " + classes.active}>
+              <div>
+                <p>Date updated: {movie.date_updated}</p>
+                <p>Opening date: {movie.opening_date}</p>
+                <p>Publication date: {movie.publication_date}</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
