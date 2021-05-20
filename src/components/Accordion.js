ee47c70e548c2a456a25046d83b26f46a1025a8e
Reginald <reginald@dinamo.no>
2021-05-20 22:04:56 +0200

    cleaned tabs and added context

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,6 +1,6 @@
 import classes from "./Accordion.module.scss";
 import { useState } from "react";
-import { TabPills, TabSummary, TabArticles, TabDates } from "./Tabs";
+import { TabContextProvider, TabPills, TabContent } from "./Tabs";
 
 function Accordion(props) {
   const dataset = props.dataset;
@@ -32,27 +32,51 @@ function AccordionItem(props) {
     isHidden ? setIsHidden(false) : setIsHidden(true);
   }
 
-  // Create array of tab labels for use with tabIndex;
-  const tabLabels = ["Short summary", "Articles", "Dates"];
+  const handleKeyPress = (event) => {
+    event.preventDefault();
+    if (event.keyCode === 0) {
+      toggleItem();
+    }
+  };
 
   return (
-    <div key={movie.key} className={classes.item}>
+    <div key={movie.key} className={classes.item} tabIndex={1} onKeyPress={handleKeyPress}>
       <div className={classes.title} onClick={toggleItem}>
         #{parseInt(movie.key) + 1} <span>{movie.display_title}</span>
+        <small>&nbsp;{isHidden ? "(Click to open)" : "(Click to close)"}</small>
       </div>
       <div className={isHidden ? classes.inner : classes.inner + " " + classes.show}>
-        <div className={classes.tab}>
-          <div className={classes.pills}>
-            <TabPills tabs={tabLabels}></TabPills>
-          </div>
-          <div className={classes.contents}>
-          
-            <TabSummary index={0} movie={movie}></TabSummary>
-            <TabArticles index={1} movie={movie}></TabArticles>
-            <TabDates index={2} movie={movie}></TabDates>
-            
-          </div>
-        </div>
+        <TabContextProvider>
+          <TabPills labels={["Short summary", "Articles", "Dates"]}></TabPills>
+          <TabContent index={0}>
+            <img alt={movie.headline} src={movie.multimedia.src} />
+            <div>
+              <h3>{movie.display_title}</h3>
+              <small>Byline: {movie.byline}</small>
+              <p>{movie.summary_short}</p>
+            </div>
+          </TabContent>
+          <TabContent index={1}>
+            <p>
+              <a href={movie.link.url} target="_blank" rel="noreferrer">
+                {movie.link.suggested_link_text}
+              </a>
+            </p>
+          </TabContent>
+          <TabContent index={2}>
+            <div>
+              <p>
+                <strong>Date updated</strong>: {movie.date_updated}
+              </p>
+              <p>
+                <strong>Opening date</strong>: {movie.opening_date}
+              </p>
+              <p>
+                <strong>Publication date</strong>: {movie.publication_date}
+              </p>
+            </div>
+          </TabContent>
+        </TabContextProvider>
       </div>
     </div>
   );
--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,78 +1,57 @@
 import classes from "./Tabs.module.scss";
-import { useState } from "react";
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 
-const TabIndexContext = createContext(0);
 
-export function TabSummary(props) {
-  const tabIndex = useContext(TabIndexContext);
-  const movie = props.movie;
-  return (
-    <div className={props.index === tabIndex ? classes.content + " " + classes.active : classes.content}>
-      <img alt={movie.headline} src={movie.multimedia.src} />
-      <div>
-        <h3>{movie.display_title}</h3>
-        <small>Byline: {movie.byline}</small>
-        <p>{movie.summary_short}</p>
-      </div>
-    </div>
-  );
-}
+// Create context between tab and content
+const TabContext = createContext({
+  currentTab: 0,
+  changeTab: (index) => {},
+});
 
-export function TabArticles(props) {
-  const tabIndex = useContext(TabIndexContext);
-  const movie = props.movie;
-  return (
-    <div className={props.index === tabIndex ? classes.content + " " + classes.active : classes.content}>
-      <p>
-        <a href={movie.link.url} target="_blank" rel="noreferrer">
-          {movie.link.suggested_link_text}
-        </a>
-      </p>
-    </div>
-  );
-}
+export function TabContextProvider(props) {
+  const [currentTab, setCurrentTab] = useState(0);
 
-export function TabDates(props) {
-  const tabIndex = useContext(TabIndexContext);
-  const movie = props.movie;
-  return (
-    <div className={props.index === tabIndex ? classes.content + " " + classes.active : classes.content}>
-      <div>
-        <p>
-          <strong>Date updated</strong>: {movie.date_updated}
-        </p>
-        <p>
-          <strong>Opening date</strong>: {movie.opening_date}
-        </p>
-        <p>
-          <strong>Publication date</strong>: {movie.publication_date}
-        </p>
-      </div>
-    </div>
-  );
+  function changeTab(index) {
+    setCurrentTab(index);
+  }
+
+  const context = {
+    currentTab: currentTab,
+    changeTab: changeTab,
+  };
+  return <TabContext.Provider value={context}>{props.children}</TabContext.Provider>;
 }
 
 export function TabPills(props) {
-  const [tabIndex, setTabIndex] = useState(0);
-
-  function changeTab(index) {
-    setTabIndex(index);
+  const tabCtx = useContext(TabContext);
+  const changeTab = tabCtx.changeTab;
+  const currentTab = tabCtx.currentTab;
+  const labels = props.labels;
+  const handleKeyPress = (event) => {
+    event.preventDefault();
+    if(event.keyCode === 0 || event.key === " "){
+      event.target.click();
+    }
   }
-
-  const tabLabels = props.tabs;
   return (
-    
-      <div className={classes.pills}>
-        {tabLabels.map((tab, index) => {
-          // render the tabs
-          return (
-            <span key={index} className={tabIndex === index ? classes.pill + " " + classes.active : classes.pill} onClick={() => changeTab(index)}>
-              {tab}
-            </span>
-          );
-        })}
-      </div>
-    
+    <div className={classes.pills}>
+      {labels.map((label, index) => {
+        return (
+          <span onKeyPress={handleKeyPress} key={index} className={currentTab === index ? classes.pill + " " + classes.active : classes.pill} onClick={()=>{changeTab(index)}} tabIndex={1}>
+            {label}
+          </span>
+        );
+      })}
+    </div>
   );
 }
+
+export function TabContent(props) {
+  const tabCtx = useContext(TabContext);
+  const currentTab = tabCtx.currentTab;
+  return <div className={props.index === currentTab ? classes.content + " " + classes.active : classes.content}>{props.children}</div>;
+}
+
+
+
+export default TabContext;
